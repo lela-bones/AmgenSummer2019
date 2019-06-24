9afd7b1ba57bb7ff0f832721d21fd59b47173c56
Lela Bones <lbones1@gulls.salisbury.edu>
2019-06-24 18:54:26 -0400

    changing some things

--- viz.py
+++ viz.py
@@ -3,11 +3,30 @@ import numpy as np
 import torch
 
 losses = np.load('losses.npy', allow_pickle=True)
+test = np.load('testaccuracy.npy', allow_pickle=True)
+train = np.load('trainaccuracy.npy', allow_pickle=True)
+
 # visualization loss 
 plt.plot(losses)
 plt.xlabel("Number of iteration")
 plt.ylabel("Loss")
 plt.title("RNN: Loss vs Number of epochs in training")
-plt.savefig('Lossgraph.png')
+plt.savefig('Results/Lossgraph.png')
+plt.show()
+
+# visualization training accuracy
+plt.plot(test)
+plt.xlabel("Number of iteration")
+plt.ylabel("Test Accuracy")
+plt.title("RNN: Test Accuracy vs Number of epochs in training")
+plt.savefig('Results/TrainAccuracy.png')
+plt.show()
+
+# visualization test accuracy 
+plt.plot(test)
+plt.xlabel("Number of iteration")
+plt.ylabel("Test Accuracy")
+plt.title("RNN: Test Accuracy vs Number of epochs in training")
+plt.savefig('Results/TestAccuracy.png')
 plt.show()
 
