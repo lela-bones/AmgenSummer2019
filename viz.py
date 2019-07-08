024e7b9e2b139ab0a95aa8c6ce43be4e73ffa554
Lela Bones <lbones1@gulls.salisbury.edu>
2019-07-08 19:01:04 -0400

    visualizing

--- viz.py
+++ viz.py
@@ -2,31 +2,50 @@ import matplotlib.pyplot as plt
 import numpy as np
 import torch
 
-losses = np.load('losses.npy', allow_pickle=True)
-test = np.load('testaccuracy.npy', allow_pickle=True)
-train = np.load('trainaccuracy.npy', allow_pickle=True)
+splitz = [0, 1, 2, 3, 4]
+batch = 10
+hidden = 64
+lr = .005
+drop = 0
+for split in splitz:
+    losses = np.loadtxt('../Results/lossBi{}_{}_{}_{}_Drop{}'.format(split, batch, hidden, lr, drop))
+#    test = np.loadtxt('../Results/test{}_{}_{}_{}_Drop{}.csv'.format(split, batch, hidden, lr, drop))
+#    train = np.loadtxt('../Results/train{}_{}_{}_{}_Drop{}.csv'.format(split, batch, hidden, lr, drop))
 
-# visualization loss 
-plt.plot(losses)
-plt.xlabel("Number of iteration")
-plt.ylabel("Loss")
-plt.title("RNN: Loss vs Number of epochs in training")
-plt.savefig('Results/Lossgraph.png')
-plt.show()
+    # visualization loss 
+    #plt.ylim(0, 2.5)
+    plt.plot(losses, label = 'Split{}'.format(split), linewidth = 1)
+    plt.xlabel("Number of iteration")
+    plt.ylabel("Loss")
+    plt.legend()
+    plt.title("LSTM: Loss vs Number of epochs in training")
+    plt.savefig('../Graphs/lossBi{}_{}_{}_{}_{}.png'.format(split, batch, hidden, lr, drop))
+    plt.show()
 
-# visualization training accuracy
-plt.plot(train)
-plt.xlabel("Number of iteration")
-plt.ylabel("Train Accuracy")
-plt.title("RNN: Train Accuracy vs Number of epochs in training")
-plt.savefig('Results/TrainAccuracy.png')
-plt.show()
+#for split in splitz:
+#    losses = np.loadtxt('../Results/loss{}_{}_{}_{}_Drop{}'.format(split, batch, hidden, lr, drop))
+    train = np.loadtxt('../Results/trainBi{}_{}_{}_{}_Drop{}.csv'.format(split, batch, hidden, lr, drop))
 
-# visualization test accuracy 
-plt.plot(test)
-plt.xlabel("Number of iteration")
-plt.ylabel("Test Accuracy")
-plt.title("RNN: Test Accuracy vs Number of epochs in training")
-plt.savefig('Results/TestAccuracy.png')
-plt.show()
+    # visualization training accuracy
+    #plt.ylim(0, 1)
+    plt.plot(train, label = 'Split{}'.format(split), linewidth = 1)
+    plt.xlabel("Number of iteration")
+    plt.ylabel("Train Accuracy")
+    plt.legend()
+    plt.title("LSTM: Train Accuracy vs Number of epochs in training")
+    plt.savefig('../Graphs/trainBi{}_{}_{}_{}_{}.png'.format(split, batch, hidden, lr, drop))
+    plt.show()
+
+
+#for split in splitz:
+    test = np.loadtxt('../Results/testBi{}_{}_{}_{}_Drop{}.csv'.format(split, batch, hidden, lr, drop))
+    # visualization test accuracy 
+    #plt.ylim(0, 1)
+    plt.plot(test, label = 'Split{}'.format(split), linewidth=1)
+    plt.xlabel("Number of iteration")
+    plt.ylabel("Test Accuracy")
+    plt.legend()
+    plt.title("LSTM: Test Accuracy vs Number of epochs in training")
+    plt.savefig('../Graphs/testBi{}_{}_{}_{}_{}.png'.format(split, batch, hidden, lr, drop))
+    plt.show()
 
